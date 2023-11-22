892e1c0d35c2d2af13694f3d4bf5a6f34775915f
AFE123x <arunf400@gmail.com>
2023-11-22 10:14:00 -0500

    Added .fxml and respective controller for the ordering page

---
+++ src/main/java/com/group27/RUPizza/Speciality_PizzaController.java
@@ -0,0 +1,24 @@
+package com.group27.RUPizza;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
+public class Speciality_PizzaController {
+
+    @FXML
+    private Button Spec_Cancel;
+
+    @FXML
+    private RadioButton Spec_Large;
+
+    @FXML
+    private RadioButton Spec_Medium;
+
+    @FXML
+    private Button Spec_Next;
+
+    @FXML
+    private RadioButton Spec_Small;
+
+}
