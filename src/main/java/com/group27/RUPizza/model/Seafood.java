facba6d474b0afd2ee27a8a9b4e447a15d6745e7
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:47:34 -0500

    Update Seafood.java

--- src/main/java/com/group27/RUPizza/model/Seafood.java
+++ src/main/java/com/group27/RUPizza/model/Seafood.java
@@ -1,2 +1,19 @@
 package com.group27.RUPizza.model;public class Seafood {
+   public Seafood(){
+        super();
+        addTopping(Topping.SHRIMP);
+        addTopping(Topping.CRAB_MEATS);
+        addTopping(Topping.SQUID);
+        setSauce(Sauce.ALFREDO);
+    }
+
+    @Override
+    public void setSauce(Sauce sauce) {
+        super.setSauce(sauce);
+    }
+
+    @Override
+    public double price() {
+        return 17.99;
+    }
 }
