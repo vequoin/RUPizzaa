5c7d0cec18aa580e1b6f5034d6006810899b71dc
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:45:29 -0500

    Update Pepperoni.java

--- src/main/java/com/group27/RUPizza/model/Pepperoni.java
+++ src/main/java/com/group27/RUPizza/model/Pepperoni.java
@@ -1,2 +1,13 @@
 package com.group27.RUPizza.model;public class Pepperoni {
+  public Pepperoni(){
+        super();
+        addTopping(Topping.PEPPERONI);
+        setSauce(Sauce.TOMATO);
+    }
+
+
+    @Override
+    public double price() {
+        return 10.99;
+    }
 }
