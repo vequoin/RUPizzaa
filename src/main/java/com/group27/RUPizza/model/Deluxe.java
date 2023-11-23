750031b597869b66a5f5a89c6534b63c82ef302f
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:44:09 -0500

    Update Deluxe.java

--- src/main/java/com/group27/RUPizza/model/Deluxe.java
+++ src/main/java/com/group27/RUPizza/model/Deluxe.java
@@ -2,8 +2,18 @@ package com.group27.RUPizza.model;
 
 public class Deluxe extends Pizza{
 
+    public Deluxe(){
+        super();
+        addTopping(Topping.PEPPERONI);
+        addTopping(Topping.SAUSAGE);
+        addTopping(Topping.MUSHROOMS);
+        addTopping(Topping.GREEN_PEPPERS);
+        addTopping(Topping.ONIONS);
+        setSauce(Sauce.TOMATO);
+    }
+
     @Override
     public double price() {
-        return 0;
+        return 14.99;
     }
 }
