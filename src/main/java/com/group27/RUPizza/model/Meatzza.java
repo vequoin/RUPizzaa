9a4439d7d721ddb678e438cd8cf6d98c1979a54a
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:44:52 -0500

    Update Meatzza.java

--- src/main/java/com/group27/RUPizza/model/Meatzza.java
+++ src/main/java/com/group27/RUPizza/model/Meatzza.java
@@ -1,2 +1,13 @@
 package com.group27.RUPizza.model;public class Meatzza {
+   public Meatzza(){
+        addTopping(Topping.PEPPERONI);
+        addTopping(Topping.HAM);
+        addTopping(Topping.SAUSAGE);
+        addTopping(Topping.BEEF);
+        setSauce(Sauce.TOMATO);
+    }
+    @Override
+    public double price() {
+        return 16.99;
+    }
 }
