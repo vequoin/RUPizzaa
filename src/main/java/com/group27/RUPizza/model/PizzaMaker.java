3e392ef0a60daf96613a4119e99e4d311703910d
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:46:04 -0500

    Update PizzaMaker.java

--- src/main/java/com/group27/RUPizza/model/PizzaMaker.java
+++ src/main/java/com/group27/RUPizza/model/PizzaMaker.java
@@ -2,6 +2,14 @@ package com.group27.RUPizza.model;
 
 public class PizzaMaker {
     public static Pizza createPizza(String pizzaType) {
-        return null;
+        return switch (pizzaType) {
+            case "Deluxe" -> new Deluxe();
+            case "Supreme" -> new Supreme();
+            case "Meatzza" -> new Meatzza();
+            case "Pepperoni" -> new Pepperoni();
+            case "Seafood" -> new Seafood();
+            case "BuildYourOwn" -> new BuildYourOwn();
+            default -> throw new IllegalArgumentException("Unknown pizza type: " + pizzaType);
+        };
     }
 }
