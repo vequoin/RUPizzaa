da1959c1c8dc3c80226cc30f97ce0b160b397d17
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:48:12 -0500

    Update Supreme.java

--- src/main/java/com/group27/RUPizza/model/Supreme.java
+++ src/main/java/com/group27/RUPizza/model/Supreme.java
@@ -1,2 +1,24 @@
 package com.group27.RUPizza.model;public class Supreme {
+  public Supreme(){
+        super();
+        addTopping(Topping.SAUSAGE);
+        addTopping(Topping.PEPPERONI);
+        addTopping(Topping.GREEN_PEPPERS);
+        addTopping(Topping.ONIONS);
+        addTopping(Topping.BLACK_OLIVES);
+        addTopping(Topping.MUSHROOMS);
+        addTopping(Topping.HAM);
+        setSauce(Sauce.TOMATO);
+    }
+
+
+    @Override
+    public void setSauce(Sauce sauce) {
+        super.setSauce(sauce);
+    }
+
+    @Override
+    public double price() {
+        return 15.99;
+    }
 }
