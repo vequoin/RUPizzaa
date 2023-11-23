4a9f1290e9c8912fee5bf688b0c21ec79143864a
Digvijay Singh <79724589+vequoin@users.noreply.github.com>
2023-11-22 22:47:00 -0500

    Update Topping.java

--- src/main/java/com/group27/RUPizza/model/Topping.java
+++ src/main/java/com/group27/RUPizza/model/Topping.java
@@ -1,16 +1,20 @@
 package com.group27.RUPizza.model;
 
 public enum Topping {
-    PEPPERONI("Spicy Italian Pepperoni"),
+   PEPPERONI("Spicy Italian Pepperoni"),
     MUSHROOMS("Fresh Mushrooms"),
     ONIONS("Chopped Onions"),
     SAUSAGE("Italian Sausage"),
     BACON("Crispy Bacon"),
-    EXTRA_CHEESE("Extra Cheese"),
+    SHRIMP("Crispy Shrimp"),
     BLACK_OLIVES("Black Olives"),
     GREEN_PEPPERS("Green Bell Peppers"),
+    SQUID("Legendary Squid from the ocean deep"),
+    CRAB_MEATS("Assorted selection of crabs from Alaska"),
+    BEEF("Our grass fed beef"),
     PINEAPPLE("Tropical Pineapple"),
-    SPINACH("Fresh Spinach");
+    SPINACH("Fresh Spinach"),
+    HAM("Fresh Ham");
 
     private final String description;
 
